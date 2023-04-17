7780ba5725da831b69e52231711c682aa70c2f3a
root <root@DESKTOP-6KSL1JH>
2023-04-17 10:37:43 +0900

    prototype

---
+++ src/Unions/construction-divisions.ts
@@ -0,0 +1,15 @@
+export const constructionDivisions = new Map<string, string>([
+	["TECHDEPARTMENTWORK", "技術部作業"],
+	["PRIMARYPROCESSING", "一次加工"],
+	["CONSTRUCTION", "組み立て"],
+	["WELDING", "溶接"],
+	["PRODUCTTRANSFER", "製品移動"],
+	["FOUNDATION", "地組"],
+	["COMPLETION", "仕上げ"],
+	["PAINTING", "塗装"],
+	["LOADING", "積込み"],
+	["INSPECTION", "検査"],
+	["HARDWARE", "金物"],
+	["FIELD", "現場"],
+	["OTHERS", "その他／工事名なし"]
+])
